b00b40230350dd18445b1903338a3e2c416b1165
tsplitter <pa.tsplitter@yahoo.com>
2020-09-20 06:38:18 +0100

    Completed module

---
+++ ssg/parsers.py
@@ -0,0 +1,24 @@
+import shutil
+from typing import list
+from pathlib import Path
+class Parser:
+    extensions: list[str] = []
+
+    def valid_extension(self, extension):
+            return extension in self.extensions
+    def parse(path: Path, source: Path, dest: Path):
+        raise NotImplementedError
+    def read(self, path):
+        with open(path, "r") as file:
+            return file.read()
+    def write(self, path, dest, content, ext=".html"):
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path, "w") as file:
+            file.write(conent)
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
+
+class ResourceParser(Parser):
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+    def parse(self, path, source, dest):
+        self.copy(path, source, dest)
--- ssg/site.py
+++ ssg/site.py
@@ -3,9 +3,10 @@ from pathlib import Path
 
 
 class Site:
-    def __init__(self, source, dest):
+    def __init__(self, source, dest, parsers=None):
         self.source = Path(source)
         self.dest = Path(dest)
+        self.parsers = parsers or []
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
@@ -16,3 +17,16 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
+    def load_parser(self, extension):
+        for parser in self.parser:
+            if parser.valid_extension(extension):
+                return parser
+
+    def run_parser(self, path):
+        for parser in self. parsers:
+            if parser is not None:
+                parser.parse(path, self.source, self.dest)
+            else:
+                print("Not Implemented")
