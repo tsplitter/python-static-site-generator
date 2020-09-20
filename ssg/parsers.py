5fe7036547ae0b36169168739196d1c7c4d6b8b2
tsplitter <pa.tsplitter@yahoo.com>
2020-09-20 06:51:04 +0100

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -14,7 +14,7 @@ class Parser:
     def write(self, path, dest, content, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
         with open(full_path, "w") as file:
-            file.write(conent)
+            file.write(content)
     def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
 
