302f579f43c4f0d1d2f6f5ff8130cdb720434514
tsplitter <pa.tsplitter@yahoo.com>
2020-09-20 09:07:22 +0100

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -33,4 +33,4 @@ class Site:
                 self.error("No parser for the {} extension, file skipped!".format(path.suffix))
     @staticmethod
     def error(message):
-        sys.stderr.write("\xb[1;31m{}\n".format(message))
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
