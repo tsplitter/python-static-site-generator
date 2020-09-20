204219034e7daae135a0ea71f3cd562d4088b25b
tsplitter <pa.tsplitter@yahoo.com>
2020-09-20 07:38:08 +0100

    Completed module

--- ssg/content.py
+++ ssg/content.py
@@ -10,7 +10,7 @@ class Content(Mapping):
     def load(cls, string):
         _, fm, content =  cls.__regex.split(string, 2)
         metadata = load(fm, Loader=FullLoader)
-        return cls(metatdata, content)
+        return cls(metadata, content)
 
     def __init__(self, metadata, content):
         self.data = metadata
